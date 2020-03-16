e24a375b885cc02789f761c12d7e36e6e86f9572
Monish.N <monish.narendra@gmail.com>
2020-03-16 11:12:11 +0530

    Add files via upload

---
+++ JS/Backgroung_change.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function(){
+var header = $('body');
+
+var backgrounds = new Array(
+    'url(../../Registration Page/Login/Main/Images/black-and-white-city-skyline-buildings.jpg)'
+  , 'url(../../Registration Page/Login/Main/Images/landscape-mountains-nature-clouds.jpg)'
+  , 'url(../../Registration Page/Login/Main/Images/pexels-photo.jpg)'
+  , 'url(http://placekitten.com/400)'
+);
+
+var current = 0;
+
+function nextBackground() {
+    current++;
+    current = current % backgrounds.length;
+    header.css('background-image', backgrounds[current]);
+}
+setInterval(nextBackground, 1000);
+
+header.css('background-image', backgrounds[0]);
+});
---
+++ Main(Home) Page/php/main_page.php
@@ -0,0 +1,41 @@
+﻿<?php
+
+if ( ! empty( $_POST ) )
+{
+$mysqli = new mysqli( 'localhost', 'root', '', 'm_m' );
+if ( $mysqli->connect_error )
+{
+die('Connect Error:' . $mysqli->connect_error . ':' . $mysqli->connect_error);
+}
+$sql = "INSERT INTO main_page (	Firstname,LastName,Email_id) VALUES 
+(
+ '{$mysqli->real_escape_string($_POST['Firstname'])}',
+ '{$mysqli->real_escape_string($_POST['LastName'])}',
+ '{$mysqli->real_escape_string($_POST['Email_id'])}'
+ )";
+
+$insert = $mysqli->query($sql);
+
+if ( $insert )
+{
+echo "Success! Row ID: {$mysqli->insert_id}";
+
+echo '<center><h1 style="font-size:80px;">Congradulation</h1></center>';
+echo '<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">';
+echo '<center><i class="fa fa-check-circle-o" style="font-size:80px;">Thank You</i></center>';
+}
+else
+{
+die("Error: {$mysqli->errno} : {$mysqli->error}");
+}
+$mysqli->close();
+} 
+?>
+
+
+
+
+
+
+
+
